61c7dc221f0161ec6eadc7cc21532c48e575ec50
Tran Quy Dong <tranquydong98@gmail.com>
2019-12-03 23:01:18 +0700

    update Serve and Client1

--- Client/client.java
+++ Client/client.java
@@ -6,47 +6,44 @@ public class client {
 	private static String serverIpAddr;
 
 	public static void main(String[] args){
+		Socket socket = null;
 		try {
 	        Scanner input = new Scanner(System.in);	
-	        System.out.print("\nServer's IP Address: ");
+	        System.out.print("Server's IP Address: ");
 	        serverIpAddr = input.nextLine();
 	        InetAddress serverAddr = InetAddress.getByName(serverIpAddr);
 	
-	    	Socket socket = new Socket(serverAddr,8888);
+	    	socket = new Socket(serverAddr,8888);
         
-       
-	       	DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-        
-	       	DataInputStream dis = new DataInputStream(socket.getInputStream());
+		DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+       		
+		DataInputStream dis = new DataInputStream(socket.getInputStream());
 		
 			System.out.print("\nConnect succesfull!");
 
-			int flag = dis.readInt();
-
-			System.out.println("\nMy flag: "+flag);
-
 			String fileName = dis.readUTF();
 
-			if(flag==1){
-				long fileSize = dis.readLong();
-				System.out.println("We will receive  " +fileName+ " direct");
-				System.out.println("\nSize of file: " +fileSize+ " bytes");
-				FileOutputStream fileOutput = new FileOutputStream(fileName);
-				byte[] buff = new byte[1024];
-				long size = 0;
-				while (size < fileSize) {
-					int count;
-					count = dis.read(buff);
-					fileOutput.write(buff, 0, count);
-					size += count;
-				}
-				System.out.println("Received file successfully!");
-				fileOutput.close();				
-			} else {
-				System.out.println("\nWe will receive from other client.");
+			long fileSize = dis.readLong();
+			System.out.println("We will receive  " +fileName+ " direct");
+			System.out.println("\nSize of file: " +fileSize+ " bytes");
+			FileOutputStream fileOutput = new FileOutputStream(fileName);
+			byte[] buff = new byte[1024];
+			long size = 0;
+			while (size < fileSize) {
+				int count;
+				count = is.read(buff);
+				fileOutput.write(buff, 0, count);
+				size += count;
 			}
+			System.out.println("Received file successfully!");
+			fileOutput.close();
+			socket.close();				
+			 
 		} catch(Exception e) {
 		    e.printStackTrace();
+		}
+		finally{
+			//socket.close();
 		}
 
 	}
--- Server/server.java
+++ Server/server.java
@@ -1,42 +1,45 @@
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
+import java.util.*; 
 
 public class server {
-
+	static Vector<RequestHandler> vectorThread = new Vector<>();
+	static Vector<String> vectorAddr = new Vector<>();
 	public static void main(String[] args) {
-		try {
-			int _port = 8888;
-
-			int flag = 0;
-
+		try { 
 			int countClient = 0;
 
+			int _port = 8888;
 			ServerSocket serverSocket = new ServerSocket(_port);
-			System.out.print("\nServer is listening at port 8888.");
+			System.out.println ("Server is listening at port 8888.");
 			while (true) {
 				Socket socket = serverSocket.accept();
 
-				DataInputStream dis = new DataInputStream(socket.getInputStream());
+				//DataInputStream dis = new DataInputStream(socket.getInputStream());
 
-				DataOutputStream dos = new DataOutputStream(socket.getOutputStream()); 
+				//DataOutputStream dos = new DataOutputStream(socket.getOutputStream()); 
 
 				InetAddress clientIp = socket.getInetAddress();
 				int clientPort = socket.getPort();
 
 				// only the first client connect have flag = 1 
-				flag += 1;
 
 				countClient += 1;
+				RequestHandler requestHandler = new RequestHandler(socket, countClient);
 				System.out.println("\nClient address: " + clientIp.toString().substring(1) + ":" + clientPort + " is connecting");
+				vectorThread.add(requestHandler);
+				vectorAddr.add(clientIp.toString());
+				requestHandler.start();
 
-				Thread requestHandler = new RequestHandler(socket, flag, countClient, dis, dos);
-	        	requestHandler.start();
-
-
+				if(countClient == 3){
+					countClient = 0;
+				}
+	
+			}
 		}
-	} catch (Exception e) {
-		    		e.printStackTrace();
+		catch (Exception e) {
+		    	e.printStackTrace();
 		}
 	}
 
@@ -44,23 +47,24 @@ public class server {
 
 class RequestHandler extends Thread {
 	
-	private static Socket socket;
-
-	private static int flag;
+	private Socket socket;
 
 	private static int countClient;
+	private InputStream is;
+	private DataInputStream dis;
+	private OutputStream os;
+	private DataOutputStream dos;
+	
+ 
+	private int buffer = 1024;
 
-	private static DataInputStream dis;
-
-	private static DataOutputStream dos;
-
-	public RequestHandler(Socket _socket, int _flag, int _countClient,DataInputStream _dis, DataOutputStream _dos) {			this.socket = _socket;
-		this.flag = _flag;
-		this.dis = _dis;
-		this.dos = _dos;
+	public RequestHandler(Socket _socket, int _countClient){//,DataInputStream _dis, DataOutputStream _dos) {			
+		//this.dis = _dis;
+		//this.dos = _dos;
 		this.socket = _socket;
 		this.countClient = _countClient;
 	}
+
 	public void run() {
 		try {
 			
@@ -69,56 +73,54 @@ class RequestHandler extends Thread {
 			String fileName;
 
 			File file;
-
-			System.out.println("flag of client: "+flag);
-
 			String msg = "You will recevied file from server!";
 			
-			dos.writeInt(flag);
-			dos.flush();
 			System.out.println("\nAll client:"+countClient);
-			if(countClient == 2) {
+		
+			dis =  new DataInputStream(socket.getInputStream());
+			
+ 			dos = new DataOutputStream( socket.getOutputStream());
+			
+			if(countClient == 3) {
 
 				// enter file until file is exists
 				while(true) {
-				System.out.print("\nEnter file's name to send to client: ");
-				fileName = input.nextLine();
-				file = new File(fileName);
+					System.out.print("\nEnter file's name to send to client: ");
+					fileName = input.nextLine();
+					file = new File(fileName);
 
-				if(file.exists()){
-					break;
-				} else {
-					System.out.print("\nFile is not found!");
-				}
-
-				}
-
-				dos.writeUTF(fileName);
-				dos.flush();
-
-				if(flag == 1) {
-					System.out.println("/nSend direct!");
-
-					long fileSize = file.length();
-					dos.writeLong(fileSize);
-					dos.flush();
-					
-					FileInputStream fileInput =  new FileInputStream(file);
-					byte[] buff = new byte[1024];
-
-					int count;	
-					while ((count = fileInput.read(buff)) != -1) {
-						dos.write(buff, 0, count);
-						dos.flush();
+					if(file.exists()){
+						break;
+					}	 	
+					else {
+						System.out.print("\nFile is not found!");
 					}
-					fileInput.close();
-					System.out.println("\nSent file successfully!");
-
-
-				} else {
-				// do somethings
-					System.out.println("\nDon't send direct!");
 				}
+				RequestHandler client1 = server.vectorThread.get(0);
+				client1.dos.writeUTF(fileName);
+				client1.dos.flush();
+
+				System.out.println("\nSend direct!");
+				long fileSize = file.length();
+				client1.dos.writeLong(fileSize);
+				client1.dos.flush();
+				
+				FileInputStream fileInput =  new FileInputStream(file);
+				byte[] buff = new byte[buffer];
+
+				int count;	
+				while ((count = fileInput.read(buff)) != -1) {
+					client1.dos.write(buff, 0, count);
+				}
+				client1.os.flush();
+				fileInput.close();
+				System.out.println("\nSent file successfully!");
+
+
+				//} else {
+				//	 do somethings
+				//	System.out.println("\nDon't send direct!");
+				//}
 
 			}
 			
